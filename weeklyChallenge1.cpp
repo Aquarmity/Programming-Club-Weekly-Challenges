b6da07b475b88651631bbb4aff845ab19a556568
Miguel Diaz <migueldiaz161@gmail.com>
2021-09-04 00:03:32 -0700

    Create weeklyChallenge1.cpp

---
+++ weeklyChallenge1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int num;
+  int space;
+
+  while (true)
+  {
+    cin >> num;
+    space = num;
+
+    if (num > 100)
+    {
+      cout << "Woah there buddy, that's a biiiiig number. Calm down fella. \n";
+    }
+    else
+    {
+      cout << num << "\n";
+      for (int i = 0; i <= num; i++)
+      {
+        string spaceTri (space--, ' ');
+        cout << spaceTri;
+
+        string tri (i * 2 + 1, '*');
+        cout << tri << "\n";
+      }
+    }
+  }
+}
